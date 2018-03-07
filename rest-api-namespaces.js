8731d698579858902dda32ce45be4151f6e3b999
Joe Hoyle <joehoyle@gmail.com>
2018-03-07 10:07:42 -0500

    coutn avg

--- rest-api-namespaces.js
+++ rest-api-namespaces.js
@@ -2,18 +2,16 @@ const fetch = require( 'isomorphic-fetch' );
 const fs = require( 'fs' );
 const data = String( fs.readFileSync( './data.csv' ) );
 
-const versions = { errored: 0 };
-
 const urls = data.split( "\n" ).map( line => line.split( ' ' )[0] );
+//const urls = [ 'kilovolt.dk' ]
 
 const log = () => d => { console.log( d ); return d }
 const namespaces = {};
-//const urls = [ 'kilovolt.dk' ];
+const perSite = [];
 
 urls.map( url => { return fetch( 'http://' + url )
 	.then( response => response.text() )
 	.then( html => html.match( /<link rel='https:\/\/api.w.org\/' href='([^']+)'/ )[1] )
-	.then( log() )
 	.then( url => fetch( url ) )
 	.then( response => response.json() )
 	.then( data => data.namespaces )
@@ -22,6 +20,8 @@ urls.map( url => { return fetch( 'http://' + url )
 			namespaces[ n ] = namespaces[ n ] ? namespaces[ n ] + 1 : 1
 		})
 		console.log( namespaces )
+		perSite.push( ns.length )
+		console.log( 'Average per site ' + ( perSite.reduce( ( a, b ) => a + b, 0 ) / perSite.length ) )
 	})
 	.catch( error => console.log( error.message ) )
 })
